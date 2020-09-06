a7d6eeb555404df8180a760ea37761c012676b86
TylerGiles402 <60163396+TylerGiles402@users.noreply.github.com>
2020-09-06 05:20:40 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var prompter = prompt("What is the total tax?")
+
+var number1=25;
+var number2=0.07;
+var answer=(number1*number2)
+document.body.querySelector("#cool").innerHTML=answer;
+document.body.querySelector("#lessCool").innerHTML=prompter;
+
+var totaltaxedbill = prompt("What is the taxed bill?")
+
+var number3=1.75;
+var number4=25;
+var answer=number3+number4
+document.body.querySelector("#cool").innerHTML=answer;
+document.body.querySelector("#lessCool").innerHTML=totaltaxedbill;
+
+var tip = prompt("What is the total bill with the tip?")
+
+var number5=26.75;
+var number6=1.3;
+var answer=number5+number6
+document.body.querySelector("#cool").innerHTML=answer;
+document.body.querySelector("#lessCool").innerHTML=tip;
